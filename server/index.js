cf2b60470f470328485e6ec67c5b52fe6f7eb8f2
MichaelAmon <70228522+MichaelAmon@users.noreply.github.com>
2025-09-28 22:53:27 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -112,5 +112,5 @@ const express = require('express');
        }
      });
 
-     const PORT = process.env.PORT || 3000;
+     const PORT = process.env.PORT || 3001;
      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
