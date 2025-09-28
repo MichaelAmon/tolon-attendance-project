318d3d9f5aa980cb9925b4d1d01e2a6d308aea98
MichaelAmon <70228522+MichaelAmon@users.noreply.github.com>
2025-09-28 23:03:48 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,13 +16,17 @@ const express = require('express');
 
      function getDistance(lat1, lon1, lat2, lon2) {
        const R = 6371;
-       const dLat = (lat2 - lat1) * Math.PI / 180;
-       const dLon = (lon2 - lon1) * Math.PI / 180;
+       const dLat = toRad(lat2 - lat1);
+       const dLon = toRad(lon2 - lon1);
        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                 Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+                 Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
                  Math.sin(dLon / 2) * Math.sin(dLon / 2);
        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
        return R * c;
+     }
+
+     function toRad(value) {
+       return value * Math.PI / 180;
      }
 
      function getOfficeName(lat, long) {
